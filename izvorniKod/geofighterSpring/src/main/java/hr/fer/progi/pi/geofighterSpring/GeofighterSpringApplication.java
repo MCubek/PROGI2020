8e7b590c478fa536fa5a1d63f535e4f9bcff7289
Matej Čubek <33391902+MCubek@users.noreply.github.com>
2020-10-17 21:01:06 +0200

    Added project template

---
+++ izvorniKod/geofighterSpring/src/main/java/hr/fer/progi/pi/geofighterSpring/GeofighterSpringApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.pi.geofighterSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeofighterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeofighterSpringApplication.class, args);
+	}
+
+}
---
+++ izvorniKod/geofighterSpring/src/test/java/hr/fer/progi/pi/geofighterSpring/GeofighterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.pi.geofighterSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofighterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
