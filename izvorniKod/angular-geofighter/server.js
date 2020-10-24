f92ad3be8d3964c729b2a6104c38f7045a0099cb
Matej Čubek <matej.cubek@fer.hr>
2020-10-24 14:46:24 +0200

    Fixed server.js config

--- izvorniKod/angular-geofighter/server.js
+++ izvorniKod/angular-geofighter/server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/angular-geofighter'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-geofighter/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
