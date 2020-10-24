0a9f7481c8feb78fb4f456fcff73381aad4a4ff8
Matej Čubek <matej.cubek@fer.hr>
2020-10-24 10:49:21 +0000

    Cicd develop

---
+++ izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/GeoFighterApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.pi.geoFighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoFighterApplication.class, args);
+	}
+
+}
