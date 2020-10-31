81df3127bceab3aa7e3099d6213e9459f5f32c62
Matej Čubek <matej.cubek@fer.hr>
2020-10-31 22:27:50 +0100

    Auth and email finished

--- izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/controller/AuthController.java
+++ izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/controller/AuthController.java
@@ -1,17 +1,29 @@
 package hr.fer.pi.geoFighter.controller;
 
 import hr.fer.pi.geoFighter.dto.RegisterRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hr.fer.pi.geoFighter.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
 
-    @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest) {
+    private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User registration successful.", OK);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account activated Successfully", OK);
     }
 }
--- izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/service/AuthService.java
+++ izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/service/AuthService.java
@@ -1,6 +1,8 @@
 package hr.fer.pi.geoFighter.service;
 
 import hr.fer.pi.geoFighter.dto.RegisterRequest;
+import hr.fer.pi.geoFighter.exceptions.SpringGeoFighterException;
+import hr.fer.pi.geoFighter.model.NotificationEmail;
 import hr.fer.pi.geoFighter.model.User;
 import hr.fer.pi.geoFighter.model.VerificationToken;
 import hr.fer.pi.geoFighter.repository.UserRepository;
@@ -8,10 +10,12 @@ import hr.fer.pi.geoFighter.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.geolatte.geom.V;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -21,6 +25,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
+    private final MailService mailService;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -33,6 +38,9 @@ public class AuthService {
         userRepository.save(user);
 
         String token = generateVerificationToken(user);
+        mailService.sendMail(new NotificationEmail("Please activate your account.", user.getEmail(),
+                "Thank you for signing up for GeoFighter. Please click on the url below to finish your registration process:\n" +
+                        "http://localhost:8080/api/auth/accountVerification/" + token));
     }
 
     private String generateVerificationToken(User user) {
@@ -43,5 +51,18 @@ public class AuthService {
 
         verificationTokenRepository.save(verificationToken);
         return token;
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        verificationToken.orElseThrow(() -> new SpringGeoFighterException("Invalid token."));
+        fetchUserAndEnable(verificationToken.get());
+    }
+
+    private void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringGeoFighterException("User " + username + " not found."));
+        user.setEnabled(true);
+        userRepository.save(user);
     }
 }
