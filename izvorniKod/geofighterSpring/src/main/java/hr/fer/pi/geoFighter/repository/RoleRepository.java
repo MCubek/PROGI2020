8f714d2bc258554b2c13949539f273d7820bab0d
Alen Papak <alen.papak@fer.hr>
2020-11-05 15:45:45 +0100

    Dodani privilege,role repository

---
+++ izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package hr.fer.pi.geoFighter.repository;
+
+import hr.fer.pi.geoFighter.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
