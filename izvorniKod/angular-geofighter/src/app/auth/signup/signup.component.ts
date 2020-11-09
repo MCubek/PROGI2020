95988d779aea6c36fe6096d00d02a3bfbd3dea82
Matej Čubek <matej.cubek@fer.hr>
2020-11-09 14:25:43 +0100

    Implemented signup

--- izvorniKod/angular-geofighter/src/app/auth/signup/signup.component.ts
+++ izvorniKod/angular-geofighter/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {SignupRequestPayload} from './signup-request.payload';
+import {AuthService} from '../shared/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupRequestPayload: SignupRequestPayload;
+  signupForm: FormGroup;
+
+  constructor(private authService: AuthService) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  signup(){
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 
 }
