9bd1e2d93378f41b696ad7a18fc274d9b370936a
Matej Čubek <matej.cubek@fer.hr>
2020-11-09 20:53:35 +0100

    Added authguard import for later use.

--- izvorniKod/angular-geofighter/src/app/app-routing.module.ts
+++ izvorniKod/angular-geofighter/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SignupComponent} from './auth/signup/signup.component';
 import {LoginComponent} from './auth/login/login.component';
-import {HomeComponent} from "./home/home.component";
+import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './auth/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
