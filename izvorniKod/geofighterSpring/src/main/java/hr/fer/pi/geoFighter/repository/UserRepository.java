618e028720e515e743a38b8396f7e4a1ed8bb0f4
Matej Čubek <matej.cubek@fer.hr>
2020-12-22 00:56:31 +0100

    Space

--- izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/repository/UserRepository.java
+++ izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/repository/UserRepository.java
@@ -2,7 +2,6 @@ package hr.fer.pi.geoFighter.repository;
 
 import hr.fer.pi.geoFighter.dto.UserEloDTO;
 import hr.fer.pi.geoFighter.model.CartographerStatus;
-import hr.fer.pi.geoFighter.model.LocationCard;
 import hr.fer.pi.geoFighter.model.Role;
 import hr.fer.pi.geoFighter.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -28,7 +27,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "select  location_cards.name, location_cards.description, location_cards.photourl\n" +
             "from users join user_card on users.user_id=user_card.user_id join location_cards on user_card.lcd_id=location_cards.lcd_id\n" +
-            "where users.username=?1\t", nativeQuery=true)
+            "where users.username=?1\t", nativeQuery = true)
     List<ArrayList<String>> findLocationCards(String username);
 
     @Query("SELECT new hr.fer.pi.geoFighter.dto.UserEloDTO(u.username, u.wins, u.losses, u.eloScore) FROM hr.fer.pi.geoFighter.model.User u WHERE u.enabled = true ORDER BY u.eloScore DESC")
