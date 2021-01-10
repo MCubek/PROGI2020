ba0871ca15e4e70ce9db54807927ee997d2c345d
Matej Čubek <matej.cubek@fer.hr>
2021-01-10 23:36:24 +0100

    Testing framework

--- izvorniKod/geofighterSpring/src/test/java/hr/fer/pi/geoFighter/GeoFighterApplicationTests.java
+++ izvorniKod/geofighterSpring/src/test/java/hr/fer/pi/geoFighter/GeoFighterApplicationTests.java
@@ -1,8 +1,14 @@
 package hr.fer.pi.geoFighter;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GeoFighterApplicationTests {
 
+    @Test
+    void loadsGeo() {
+        Assertions.assertEquals(10, 5 + 5);
+    }
 }
