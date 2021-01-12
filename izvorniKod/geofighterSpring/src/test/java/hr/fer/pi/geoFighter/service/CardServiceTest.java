40e849e0ed58984a70eda77d56d35b0217c5e4ed
Matej Čubek <matej.cubek@fer.hr>
2021-01-12 11:21:42 +0100

    Some More tests

--- izvorniKod/geofighterSpring/src/test/java/hr/fer/pi/geoFighter/service/CardServiceTest.java
+++ izvorniKod/geofighterSpring/src/test/java/hr/fer/pi/geoFighter/service/CardServiceTest.java
@@ -1,18 +1,64 @@
 package hr.fer.pi.geoFighter.service;
 
-import org.junit.jupiter.api.Test;
+import hr.fer.pi.geoFighter.exceptions.SpringGeoFighterException;
+import hr.fer.pi.geoFighter.model.LocationCard;
+import hr.fer.pi.geoFighter.model.User;
+import hr.fer.pi.geoFighter.model.UserCard;
+import hr.fer.pi.geoFighter.model.UserCardId;
+import hr.fer.pi.geoFighter.repository.LocationCardRepository;
+import hr.fer.pi.geoFighter.repository.UserCardRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.*;
 
 import java.awt.geom.Point2D;
 import java.time.Instant;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
 /**
  * @author MatejCubek
  * @project pi
  * @created 11/01/2021
  */
+@ExtendWith(MockitoExtension.class)
 class CardServiceTest {
+
+    @InjectMocks
+    private CardService cardService;
+
+    @Mock
+    private LocationCardRepository locationCardRepository;
+
+    @Mock
+    private UserCardRepository userCardRepository;
+
+    @Mock
+    private AuthService authService;
+
+    private LocationCard locationCard;
+
+    private User currentUser;
+
+    @BeforeEach
+    void initCard() {
+        locationCard = new LocationCard();
+        locationCard.setLocation(new Point2D.Double(0, 0));
+        locationCard.setId(1L);
+        locationCard.setName("card");
+
+        currentUser = new User();
+        currentUser.setUserId(1L);
+        currentUser.setUsername("user");
+
+        when(locationCardRepository.getLocationCardById(1L)).thenReturn(Optional.of(locationCard));
+        when(authService.getCurrentUser()).thenReturn(currentUser);
+        when(userCardRepository.findById(new UserCardId(currentUser.getUserId(), locationCard.getId()))).thenReturn(Optional.empty());
+    }
 
     @Test
     void getLocationString() {
@@ -32,5 +78,41 @@ class CardServiceTest {
         Instant time = Instant.EPOCH;
 
         assertEquals("1970-01-01 01:00", CardService.getTime(time));
+    }
+
+    @Test
+    void collectLocationCardThrowsErrorIfUserIsTooFar() {
+        currentUser.setCurrentLocation(new Point2D.Double(15, 15));
+
+        Assertions.assertThrows(SpringGeoFighterException.class, () -> cardService.collectLocationCard(1L));
+    }
+
+    @Test
+    void collectLocationCardSavesWhenValid() {
+        currentUser.setCurrentLocation(new Point2D.Double(0.01, 0.01));
+
+        cardService.collectLocationCard(1L);
+
+        var uc = new UserCard();
+        uc.setLocationCard(locationCard);
+        uc.setUser(currentUser);
+
+        verify(userCardRepository).save(uc);
+    }
+
+    @Test
+    void collectLocationCardDoesNothingWhenUserHasCard() {
+        currentUser.setCurrentLocation(new Point2D.Double(0.01, 0.01));
+
+        var uc = new UserCard();
+        uc.setLocationCard(locationCard);
+        uc.setUser(currentUser);
+
+        when(userCardRepository.findById(new UserCardId(currentUser.getUserId(), locationCard.getId()))).thenReturn(Optional.of(uc));
+
+        cardService.collectLocationCard(1L);
+
+
+        verify(userCardRepository, times(0)).save(uc);
     }
 }
