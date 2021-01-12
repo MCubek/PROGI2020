eaf72777abbc25e2abfb0db820297e742291f182
Matej Čubek <matej.cubek@fer.hr>
2021-01-12 08:47:14 +0100

    Added SUID

--- izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/exceptions/SpringGeoFighterException.java
+++ izvorniKod/geofighterSpring/src/main/java/hr/fer/pi/geoFighter/exceptions/SpringGeoFighterException.java
@@ -3,6 +3,8 @@ package hr.fer.pi.geoFighter.exceptions;
 import org.springframework.mail.MailException;
 
 public class SpringGeoFighterException extends RuntimeException {
+    private static final long serialVersionUID = 6411390069462815083L;
+
     public SpringGeoFighterException(String s, MailException e) {
         super(s, e);
     }
